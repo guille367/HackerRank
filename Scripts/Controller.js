c3e79f920f52f197ce8e2bb7fdb6a70c662a9d5d
guille367 <guilleee-@hotmail.com>
2016-02-23 17:53:19 -0300

    Tratando de filtrar los dominios de hackerrank

---
+++ Scripts/Controller.js
@@ -0,0 +1,9 @@
+var app = angular.module("MyAPp",[]);
+
+app.controller('ctrller',function($scope){
+    $scope.algoritmos = {wampa:{
+        Warmup:{data:'hola'},
+        Implementation:{},
+        Strings:{}
+    }}
+});
