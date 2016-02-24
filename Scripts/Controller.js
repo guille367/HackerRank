1508430bb393993d6e64c147c377fc64ce43636c
guille367 <guilleee-@hotmail.com>
2016-02-24 18:01:59 -0300

    asdasd

--- Scripts/Controller.js
+++ Scripts/Controller.js
@@ -1,9 +1,19 @@
 var app = angular.module("MyAPp",[]);
 
-app.controller('ctrller',function($scope){
-    $scope.algoritmos = {wampa:{
-        Warmup:{data:'hola'},
-        Implementation:{},
-        Strings:{}
-    }}
-});
+
+app.controller('ctrller',['$scope',function($scope){
+   
+    $scope.dominios = [
+        {nombre:'Algorithms',
+        subdominios:{nombre:'Warmup',nombre:'Implementation',nombre:'Strings'}
+        },
+        {nombre:'Data Structures'}
+    ];
+    
+     $scope.filtrarDominio = function(e){
+       $scope.dominiosFiltrados =  e.dominios.filter(function(dom){
+            return dom.nombre == $scope.doms;
+        });
+    }
+    
+}]);
